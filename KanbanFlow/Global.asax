6da00cbdbe2fcac3caaded985d28f28d8eb6c90a
dotnetstation <rad_hass@hotmail.com>
2013-12-03 19:06:42 +0100

    Hello world

---
+++ KanbanFlow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KanbanFlow.MvcApplication" Language="C#" %>
---
+++ KanbanFlow/Scripts/KanbanViewModel.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../Scripts/knockout-2.3.0.js"/>
+/// <reference path="../Scripts/jquery-2.0.3.js"/>
+
+function KanbanViewModel() {
+    this.title = 'My board';
+}
+
+$(document).ready(function() {
+// Activates knockout.js
+    ko.applyBindings(new KanbanViewModel());
+});
